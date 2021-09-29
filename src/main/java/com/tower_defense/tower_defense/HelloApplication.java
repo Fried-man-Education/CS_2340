36150552e5894b1f33e6914451f59e2b6331a9fc
Alik-da-Geek <aliksemelianov@gmail.com>
2021-09-29 16:01:11 -0400

    Add Configuration screen

--- src/main/java/com/tower_defense/tower_defense/HelloApplication.java
+++ src/main/java/com/tower_defense/tower_defense/HelloApplication.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("WelcomeScreen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("InitialGameScreen.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("WelcomeScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Tower Defense");
         stage.setScene(scene);
