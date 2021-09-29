80095524446ca95c4f3275e7279a69209310035c
Alik-da-Geek <aliksemelianov@gmail.com>
2021-09-29 16:26:28 -0400

    Add IntialGameScreen

--- src/main/java/com/tower_defense/tower_defense/HelloApplication.java
+++ src/main/java/com/tower_defense/tower_defense/HelloApplication.java
@@ -13,7 +13,6 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("InitialGameScreen.fxml"));
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("WelcomeScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Tower Defense");
         stage.setScene(scene);
