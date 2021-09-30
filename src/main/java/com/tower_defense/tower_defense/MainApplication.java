ac3b9578529d3ba88b177ed16bbb6b909e0ff934
fried-man <andrewfriedman01@gmail.com>
2021-09-29 23:50:13 -0400

    close #3; Add start button functionality, rename files

---
+++ src/main/java/com/tower_defense/tower_defense/MainApplication.java
@@ -0,0 +1,28 @@
+package com.tower_defense.tower_defense;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        loadStage("TitleScreen.fxml", stage);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public void loadStage(final String where, final Stage stage)  throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(where));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Tower Defense");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
