9a5ccd453256391929462f7983063ed649cbb13e
JosephVitko21 <josephvitko21@gmail.com>
2021-10-03 22:58:13 -0400

    fix checkstyle part 2

--- src/main/java/com/tower_defense/tower_defense/GameController.java
+++ src/main/java/com/tower_defense/tower_defense/GameController.java
@@ -1,18 +1,8 @@
 package com.tower_defense.tower_defense;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class GameController extends MainApplication {
 
@@ -27,23 +17,27 @@ public class GameController extends MainApplication {
         HealthLabel.setText("Health: " + difficultHealth(difficulty));
     }
 
-    public String difficultMoney (String difficulty) {
+    public String difficultMoney(String difficulty) {
         int startingMoney = 1000;
         return calculateValue(difficulty, startingMoney);
     }
 
-    public String difficultHealth (String difficulty) {
+    public String difficultHealth(String difficulty) {
         int startingHealth = 200;
         return calculateValue(difficulty, startingHealth);
     }
 
     private String calculateValue(String difficulty, int num) {
-        return switch (difficulty) {
-            case "Easy" -> String.valueOf(num);
-            case "Medium" -> String.valueOf(num / 2);
-            case "Hard" -> String.valueOf(num / 4);
-            default -> throw new IllegalArgumentException("Invalid difficulty: " + difficulty);
-        };
+        switch (difficulty) {
+        case "Easy":
+            return String.valueOf(num);
+        case "Medium":
+            return String.valueOf(num / 2);
+        case "Hard":
+            return String.valueOf(num / 4);
+        default:
+            throw new IllegalArgumentException("Invalid difficulty: " + difficulty);
+        }
     }
 
     public static String getName() {
