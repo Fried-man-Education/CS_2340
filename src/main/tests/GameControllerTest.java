1ed21592e93ed73b632c82c5eba9ae4478d0df2c
fried-man <andrewfriedman01@gmail.com>
2021-10-17 23:04:58 -0400

    Add Joe's JUNIT test

--- src/main/tests/GameControllerTest.java
+++ src/main/tests/GameControllerTest.java
@@ -107,4 +107,26 @@ public class GameControllerTest {
         assertEquals(24, (int)temp.getWidth());
         assertEquals(Color.web("0x808080ff"), temp.getFill());
     }
+
+    // Joseph Vitko's Test
+    @Test(timeout = TIMEOUT)
+    public void testGreenTile() {
+        int x = 0, y = 0;
+        Rectangle temp = gameScreen.createGreenTile(x,y);
+        assertEquals(x + "," + y, temp.getId());
+        assertEquals(20, (int)temp.getHeight());
+        assertEquals(24, (int)temp.getWidth());
+        assertEquals(Color.web("0x008000ff"), temp.getFill());
+    }
+
+    // Joseph Vitko's Test
+    @Test(timeout = TIMEOUT)
+    public void testBaseTile() {
+        int x = 0, y = 0;
+        Rectangle temp = gameScreen.createBaseTile(x,y);
+        assertEquals(x + "," + y, temp.getId());
+        assertEquals(20, (int)temp.getHeight());
+        assertEquals(24, (int)temp.getWidth());
+        assertEquals(Color.web("0x0000ffff"), temp.getFill());
+    }
 }
