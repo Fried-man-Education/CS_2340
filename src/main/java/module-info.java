4e127d0a1e61c5323561ba829d8cd9f8e00dfccc
fried-man <andrewfriedman01@gmail.com>
2021-10-31 22:01:09 -0400

    fix import issues

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.tower_defense.tower_defense {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires com.almasb.fxgl.all;
+    requires annotations;
     requires junit;
 
 
