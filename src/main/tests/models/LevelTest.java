d7eb803748d355943a826f6dd35c63cb28aae0d0
Joseph Vitko <josephvitko21@gmail.com>
2021-11-03 01:32:12 -0400

    fix checkstyle

--- src/main/tests/models/LevelTest.java
+++ src/main/tests/models/LevelTest.java
@@ -12,8 +12,8 @@ import static org.junit.Assert.*;
 public class LevelTest {
     private Level level;
     @Before
-    public void setup(){
-        Point2D randPoint = new Point2D(1,1);
+    public void setup() {
+        Point2D randPoint = new Point2D(1, 1);
         ArrayList<Point2D> randWayPoints = new ArrayList<>();
         int index = 1;
         int numEnemies = 10;
@@ -22,33 +22,33 @@ public class LevelTest {
 
     //#11 Alik Emelianov
     @Test
-    public void testGetSpawnPoint(){
-        Point2D randSpawnPoint = new Point2D(1,1);
+    public void testGetSpawnPoint() {
+        Point2D randSpawnPoint = new Point2D(1, 1);
         Point2D returnedSpawnPoint = level.getSpawnPoint();
         assertEquals(randSpawnPoint, returnedSpawnPoint);
     }
 
     //#12 Alik Emelianov
     @Test
-    public void testSetSpawnPoint(){
-        Point2D newSpawnPoint = new Point2D(10,20);
+    public void testSetSpawnPoint() {
+        Point2D newSpawnPoint = new Point2D(10, 20);
         level.setSpawnPoint(newSpawnPoint);
         assertEquals(newSpawnPoint, level.getSpawnPoint());
     }
 
     //#13 Alik Emelianov
     @Test
-    public void testGetWayPoints(){
+    public void testGetWayPoints() {
         ArrayList<Point2D> sameWayPoints = new ArrayList<>();
         assertEquals(sameWayPoints, level.getWayPoints());
     }
 
     //#14 Andrew Friedman
     @Test
-    public void testSetWayPoints(){
+    public void testSetWayPoints() {
         ArrayList<Point2D> newWayPoints = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
-            Point2D wayPoint = new Point2D(i,i);
+        for (int i = 0; i < 10; i++) {
+            Point2D wayPoint = new Point2D(i, i);
             newWayPoints.add(wayPoint);
         }
         level.setWayPoints(newWayPoints);
@@ -57,14 +57,14 @@ public class LevelTest {
 
     //#15 Andrew Friedman
     @Test
-    public void testSetActiveLevel(){
+    public void testSetActiveLevel() {
         Level.setActiveLevel(level);
         assertEquals(level, Level.getActiveLevel());
     }
 
     //#16 Andrew Friedman
     @Test
-    public void testGetIndex(){
+    public void testGetIndex() {
         assertEquals(1, level.getIndex());
     }
 }
