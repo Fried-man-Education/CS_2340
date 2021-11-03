a80d83b40a31edf31614900d53608879a493bffa
Alik-da-Geek <aliksemelianov@gmail.com>
2021-11-03 00:55:02 -0400

    Add first 10 tests

--- src/main/tests/models/CombatTest.java
+++ src/main/tests/models/CombatTest.java
@@ -1,4 +1,20 @@
 package models;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.theswagbois.towerdefense.models.Combat;
 
 public class CombatTest {
+    //#9 Alik Emelianov
+    @Test
+    public void testCombatStarted(){
+        Combat.setCombatStarted(true);
+        assertTrue(Combat.isCombatStarted());
+    }
+
+    //#10 Alik Emelianov
+    @Test
+    public void testCombatEnded(){
+        Combat.setCombatStarted(false);
+        assertFalse(Combat.isCombatStarted());
+    }
 }
