191ff2d9482e1f04aea6780facf32c0e8caec7eb
Andrew Friedman <andrewfriedman01@gmail.com>
2021-11-14 22:26:59 -0500

    Add 3 JUNIT enemies tests

---
+++ src/main/tests/services/EnemiesTest.java
@@ -0,0 +1,51 @@
+package services;
+
+import com.theswagbois.towerdefense.models.Level;
+import com.theswagbois.towerdefense.models.Player;
+import com.theswagbois.towerdefense.services.Enemies;
+import com.theswagbois.towerdefense.services.Levels;
+import com.theswagbois.towerdefense.services.Towers;
+import com.theswagbois.towerdefense.ui.GamePanel;
+import javafx.geometry.Point2D;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class EnemiesTest {
+    private static final int TIMEOUT = 200;
+    private Enemies enemies;
+
+    @Before
+    public void setup() {
+        enemies = new Enemies();
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testGetNumEnemies() {
+        assertEquals(4, enemies.getNumEnemies());
+        enemies.setNumEnemies(5);
+        assertEquals(5, enemies.getNumEnemies());
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testSetNumEnemies() {
+        assertEquals(3, enemies.getNumEnemies());
+        enemies.setNumEnemies(4);
+        assertEquals(4, enemies.getNumEnemies());
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testGetEnemiesData() {
+        assertEquals(new ArrayList<>(), enemies.getEnemiesData());
+    }
+
+    /*@Test(timeout = TIMEOUT)
+    public void testLoadEnemiesData() {
+        assertEquals(new ArrayList<>(), enemies.getEnemiesData());
+        enemies.loadEnemiesData();
+        assertNotEquals(new ArrayList<>(), enemies.getEnemiesData());
+    }*/
+}
