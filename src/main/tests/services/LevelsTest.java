c97fff8eec3fb473368e3944aeb484b5bda99c31
Andrew Friedman <andrewfriedman01@gmail.com>
2021-11-14 22:32:26 -0500

    Add 3 JUNIT levels tests

---
+++ src/main/tests/services/LevelsTest.java
@@ -0,0 +1,39 @@
+package services;
+
+import com.theswagbois.towerdefense.models.Level;
+import com.theswagbois.towerdefense.services.Levels;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class LevelsTest {
+    private static final int TIMEOUT = 200;
+    private Levels levels;
+
+    @Before
+    public void setup() {
+        levels = new Levels();
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testGetLevels() {
+        assertEquals(new ArrayList<Level>(), levels.getLevels());
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testGetLevelCount() {
+        assertEquals(4, levels.getLevelCount());
+        levels.setLevelCount(5);
+        assertEquals(5, levels.getLevelCount());
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testSetLevelCount() {
+        assertEquals(3, levels.getLevelCount());
+        levels.setLevelCount(4);
+        assertEquals(4, levels.getLevelCount());
+    }
+}
